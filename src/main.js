5d3b05648b3e3c1370d87eb7a2834fc92643ffe8
Felipe Puentes <feliipepuentes@gmail.com>
2023-07-18 16:35:02 -0500

    adding contact view

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faSquareGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelopeOpen, faPhone } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faSquareGithub, faLinkedin)
+library.add(faSquareGithub, faLinkedin, faEnvelopeOpen, faPhone)
 
 createApp(App).use(router).component('fa', FontAwesomeIcon).mount('#app')
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,7 +1,57 @@
 <template>
   <appearing-transition appear>
-    <div v-if="showContent">
-      <p>Contact me</p>
+    <div v-if="showContent" class="contact-view">
+      <div>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <h6 class="contact-label">
+                  <fa icon="fa-solid fa-envelope-open" />
+                  Email
+                </h6>
+              </td>
+              <td>
+                <div class="link">
+                  <a
+                    href="mailto:of28guino@gmail.com"
+                    target="_blank"
+                    rel="noreferrer"
+                    class="contact-a"
+                  >
+                    ↗ of28guino@gmail.com</a
+                  >
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <h6 class="contact-label">
+                  <fa icon="fa-solid fa-phone" />
+                  Phone Number
+                </h6>
+              </td>
+              <td>
+                <div class="link">
+                  <a
+                    href="tel:+573112473146"
+                    target="_blank"
+                    rel="noreferrer"
+                    class="contact-a"
+                    >↗ +57 311 247 31 46</a
+                  >
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </appearing-transition>
 </template>
@@ -16,4 +66,30 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.contact-view {
+  position: relative;
+  margin-left: -10%;
+}
+
+.link {
+  width: auto;
+  margin-bottom: 3px;
+  white-space: nowrap;
+  margin-left: 5%;
+}
+
+.contact-a {
+  font-size: 1.25rem;
+  font-variation-settings: 'wght' 500, 'wdth' 125;
+  color: var(--color);
+  text-transform: lowercase;
+  text-decoration: none;
+}
+
+.contact-label {
+  text-transform: uppercase;
+  font-variation-settings: 'wght' 800, 'wdth' 125;
+  font-size: 1.25rem;
+}
+</style>
